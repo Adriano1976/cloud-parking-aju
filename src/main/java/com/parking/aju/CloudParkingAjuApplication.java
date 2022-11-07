72b682a4079db4ae5cbafdd73fc01f9ee11d77f4
Adriano Santos <adriradiologista@hotmail.com.br>
2022-11-07 16:44:46 -0300

    Gerando aplicação com Spring Initializr.

---
+++ src/main/java/com/parking/aju/CloudParkingAjuApplication.java
@@ -0,0 +1,13 @@
+package com.parking.aju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkingAjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudParkingAjuApplication.class, args);
+    }
+
+}
